64cd24470cf783fd629fa55b539570273f252570
Sebastiano <160122164+Sebahhhhh@users.noreply.github.com>
2024-09-25 19:16:22 +0200

    Add files via upload
    Aggiunta la possibilità di cambiare il formato + aggiunti alcuni commenti

--- Main.java
+++ Main.java
@@ -2,32 +2,49 @@ import java.util.Scanner;
 
 public class Main {
 
+    //non permette di visionare una lista dei brani salvati
+
     public static void main(String[] args) {
 
         // Scanner
         Scanner in = new Scanner(System.in);
 
-        
+        // Input informazioni sul brano
         System.out.print("Inserisci il titolo del brano: ");
         String titolo = in.nextLine();
 
         System.out.print("Inserisci l'autore del brano: ");
         String autore = in.nextLine();
 
-        System.out.print("Inserisci il formato del brano");
+        System.out.print("Inserisci il formato del brano | Es: .mp3, .wav: ");
         String formato = in.nextLine();
 
         // Creazione di un brano
         BranoDigitale br = new BranoDigitale(formato, titolo, autore);
         // br = brano
-        
+
         // Stampa delle informazioni del brano
         System.out.println("\nInformazioni del brano:");
         System.out.println("Titolo: " + br.getTitolo());
         System.out.println("Autore: " + br.getAutore());
         System.out.println("Formato: " + br.getFormato());
+
+        // Cambio formato
+        System.out.print("\nVuoi cambiare il formato del brano? (Y/N): ");
+        String risposta = in.nextLine();
+
+        if (risposta.equalsIgnoreCase("Y")) {
+            System.out.print("Inserisci il nuovo formato: ");
+            String n_Formato = in.nextLine();
+            br.setFormato(n_Formato);
+            System.out.println("Il formato è stato aggiornato a: " + br.getFormato());
+        } else {
+            System.out.println("Il formato non è stato cambiato.");
+        }
+
+    }
 }
-}
+
 // Classi
 class BranoDigitale {
 
@@ -62,3 +79,4 @@ class BranoDigitale {
         formato = nuovoFormato;
     }
 }
+
